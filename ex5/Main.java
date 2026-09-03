d2dc0d02de0a8e667cfc30aaf4bcc0f0425abb47
Biazinha001 <bm1744420@gmail.com>
2026-09-02 22:17:50 -0300

    atalizacao5

--- ex5/Main.java
+++ ex5/Main.java
@@ -2,39 +2,57 @@ package ex5;
 
 public class Main {
     public static void main (String[] args) {
-            String[] alunos = {"Miranata", "Savalo", "Aeronauta"};
+            //String[] alunos = {"Miranata", "Savalo", "Aeronauta"};
 
-            alunos[0] = "Mariazinha";
-            System.out.println("Qtde de Alunos; " + alunos.length);
+            //alunos[0] = "Mariazinha";
+            //System.out.println("Qtde de Alunos; " + alunos.length);
     
-            for (String estudante : alunos){
-                System.out.println(estudante);
-            }
+            //for (String estudante : alunos){
+                //System.out.println(estudante);
+            //}
 
             //Crie uma lista com 5 produtos
             //exiba cada produto utilizando foreach
             //e também for
             
-                    String[] produtos = {
-                        "Shampoo",
-                        "Condicionador",
-                        "Mascara de hidratacao",
-                        "Creme de pentear",
-                        "oleo capilar"
-                    };
+                    //String[] produtos = {
+                       // "Shampoo",
+                        //"Condicionador",
+                       // "Mascara de hidratacao",
+                        //"Creme de pentear",
+                        //"oleo capilar"
+                   // };
             
-                    System.out.println("Com foreach:");                    
-                    for (String produto : produtos) {
-                        System.out.println(produto);
-                    }
+                    //System.out.println("Com foreach:");                    
+                    //for (String produto : produtos) {
+                        //System.out.println(produto);
+                    //}
             
-                    System.out.println("\nCom for:");
+                    //System.out.println("\nCom for:");
             
-                    for (int i = 0; i < produtos.length; i++) {
-                        System.out.println(produtos[i]);
-                    }
-                }
-            }
+                    //for (int i = 0; i < produtos.length; i++) {
+                        //System.out.println(produtos[i]);
+                   // }
+
+                    //Crie um array contendo 5 números
+                    //Use foreach para exibir se cada número é
+                    //Positivo, negativo ou igual a zero
+        
+                            int[] numeros = {10, -5, 0, 7, -2};
+                    
+                            for (int numero : numeros) {
+                                if (numero > 0) {
+                                    System.out.println(numero + " e positivo");
+                                } else if (numero < 0) {
+                                    System.out.println(numero + " e negativo");
+                                } else {
+                                    System.out.println(numero + " e igual a zero");
+                                }
+                            }
+                    
+                    
+                        }
+                    }  
             
             
         
