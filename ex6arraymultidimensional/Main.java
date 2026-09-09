dfc7ecec4a1582c3895e56a599ccd1130689a32f
Biazinha001 <bm1744420@gmail.com>
2026-09-09 20:29:45 -0300

    atualizacao6

---
+++ ex6arraymultidimensional/Main.java
@@ -0,0 +1,30 @@
+package ex6arraymultidimensional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Crie uma matriz com 4 linhas e 4 colunas.
+        //Para cada posição da matriz, preencha com valores inteiros aleaórios
+        //Exiba o valor da posição 2,3
+        
+        int[][] numeros= {
+            {1, 4, 2, 8},
+            {3, 6, 8, 9},
+            {4, 7, 5, 3},
+            {9, 2, 1, 3}
+        };
+        for(int i = 0; 1 < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+
+            }
+        //for(int 1 = 0; 1 < 4; 1++){
+        //    for(int j = 0; j < 4; j++)
+        //    System.out.println(numeros[i][j]);
+        }
+        //System.out.println(numeros[2][3]);
+    }
+ }
+        
+        
+    
+
